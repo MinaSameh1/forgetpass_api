f888d256dcc93ff327cb98cb2a0ae492bb4bab96
MinaSameh1 <47836846+MinaSameh1@users.noreply.github.com>
2022-06-26 01:56:31 +0200

    fixed buggs

--- src/app/token/token.repository.ts
+++ src/app/token/token.repository.ts
@@ -1,20 +1,45 @@
-import { FilterQuery, QueryOptions } from "mongoose";
-import { string } from "zod";
+import { FilterQuery, QueryOptions, QuerySelector } from "mongoose";
 import TokenModel, { tokenDocuement } from "./token.model";
 
-export function findToken(uid: string, options: QueryOptions = { lean: true }) {
-  return TokenModel.findOne({ uid: uid }, {}, options)
-}
+// Function overloads
+// TODO Remove this ANY!
+export function findToken(query: FilterQuery<tokenDocuement>, select?: QuerySelector<tokenDocuement>, options?: QueryOptions<tokenDocuement>): any
+export function findToken(query: FilterQuery<tokenDocuement>): any
+export function findToken(query: FilterQuery<tokenDocuement>, options?: QueryOptions<tokenDocuement>): any
+export function findToken(query: string): any
 
-export function findTokenQuery(query: FilterQuery<tokenDocuement>, options: QueryOptions = { lean: true }) {
-  return TokenModel.findOne(query, {}, options)
+export function findToken(
+  query: FilterQuery<tokenDocuement> | string,
+  select?: unknown,
+  options?: unknown
+) {
+  let Select: QuerySelector<tokenDocuement> = {}
+  let Options: QueryOptions<tokenDocuement> = {}
+  if (select) {
+    Select = select as QuerySelector<tokenDocuement>
+    Options = options as QueryOptions<tokenDocuement>
+    if (typeof query === "string") {
+      return TokenModel.findOne({ token: query }, Select, Options || { lean: true })
+    }
+    else {
+      return TokenModel.findOne(query, Select, Options || { lean: true })
+    }
+  } else {
+    Options = options as QueryOptions<tokenDocuement>
+    if (typeof query === "string") {
+      return TokenModel.findOne({ token: query }, {}, Options || { lean: true })
+    }
+    else {
+      return TokenModel.findOne(query, {}, Options || { lean: true })
+    }
+  }
 }
 
 export function createToken({
   uid,
   token
 }: {
-  uid:string,
+  uid: string,
   token: string
 }) {
   return TokenModel.create({ token: token, uid: uid })
